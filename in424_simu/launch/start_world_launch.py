488b6ade3b1509a247ea21d36b67ed8459abaec0
JohvanyROB <johvany.gustave@gmail.com>
2024-02-28 11:45:15 +0100

    gazebo executeprocess

--- in424_simu/launch/start_world_launch.py
+++ in424_simu/launch/start_world_launch.py
@@ -4,19 +4,13 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    gazebo_ros_pkg = get_package_share_directory("gazebo_ros")
     simu_pkg = get_package_share_directory("in424_simu")
 
-    gazebo_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(gazebo_ros_pkg, "launch", "gazebo.launch.py")
-        )
-    )
     os.environ["GAZEBO_MODEL_PATH"] = os.path.join(simu_pkg, "models")
 
     spawn_robots_launch = IncludeLaunchDescription(
@@ -26,14 +20,20 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            "world",
-            default_value = os.path.join(simu_pkg, "worlds", "env.world"),
-            description = "World file to use for the simulation"
-        ),
+        ExecuteProcess(cmd=[
+            'gazebo',
+            # '--verbose',
+            '-s', 'libgazebo_ros_init.so',  # Publish /clock
+            '-s', 'libgazebo_ros_factory.so',  # Provide gazebo_ros::Node
+            os.path.join(simu_pkg, "worlds", "env.world")
+        ], output='screen'),
 
-        gazebo_launch,
-        spawn_robots_launch,
+        TimerAction(
+           period = 5.0,
+           actions = [
+                spawn_robots_launch
+           ]
+        ),
 
         Node(
             package = "rviz2",
