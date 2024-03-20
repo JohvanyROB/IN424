85f304605cfe597049aa98fc8c11c5524a9c51e6
JohvanyROB <johvany.gustave@gmail.com>
2024-03-20 09:40:21 +0100

    launch file updated

--- in424_simu/launch/start_world_launch.py
+++ in424_simu/launch/start_world_launch.py
@@ -4,14 +4,22 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, ExecuteProcess, TimerAction, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     simu_pkg = get_package_share_directory("in424_simu")
+    gazebo_pkg = get_package_share_directory("gazebo_ros")
 
     os.environ["GAZEBO_MODEL_PATH"] += os.path.join(simu_pkg, "models")
+
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(gazebo_pkg, "launch", "gazebo.launch.py")
+        )
+    )
 
     spawn_robots_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -19,14 +27,28 @@ def generate_launch_description():
         )
     )
 
+    stop_previous_gazebo = ExecuteProcess(cmd=[
+        "killall",
+        "gzserver",
+        "gzclient"
+    ], output='screen')
+
+
     return LaunchDescription([
-        ExecuteProcess(cmd=[
-            'gazebo',
-            # '--verbose',
-            '-s', 'libgazebo_ros_init.so',  # Publish /clock
-            '-s', 'libgazebo_ros_factory.so',  # Provide gazebo_ros::Node
-            os.path.join(simu_pkg, "worlds", "env.world")
-        ], output='screen'),
+        DeclareLaunchArgument(
+            "world",
+            default_value = os.path.join(simu_pkg, "worlds", "env.world"),
+            description = "World file to use for the simulation"
+        ),
+
+        stop_previous_gazebo,
+
+        RegisterEventHandler(
+            event_handler = OnProcessExit(
+              target_action = stop_previous_gazebo,
+              on_exit = [gazebo_launch]  #start this node when previous gazebo processes have been stopped
+            )
+        ),
 
         TimerAction(
            period = 5.0,
